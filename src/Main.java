5dd8be0f4224fd1e6b358716de8420614ad58336
cdominguez_axity <cdominguez.bravo@gmail.com>
2020-02-15 19:25:10 -0300

    trabajando con arreglos

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ public class Main {
 
 
   public static void main(String[] args) {
-
+    int arreglo[] = new int[10];
+    arreglo[4] = 9;
+    for(int i=0; i < 10; i++){
+      System.out.println(arreglo[i]);
+    }
+    System.out.println(arreglo.length);
   }
 }
