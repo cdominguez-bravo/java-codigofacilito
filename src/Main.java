f7ad43dd4fc8c0fe28e8d3b021525ecf86f45aa1
cdominguez_axity <cdominguez.bravo@gmail.com>
2020-02-15 19:10:05 -0300

    trabajando con strings

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,12 @@ public class Main {
 
 
   public static void main(String[] args) {
-    String x = "Hola mundo !!!p";
-    System.out.println(x.charAt(6));
-    System.out.println(x.charAt(x.length()-1));
-    for (int i = 0; i <x.length();i++){
-      System.out.println(x.charAt(i));
-
-    }
-    int c = x.indexOf("m");
-    System.out.println(c);
+    String s1 = "Hola";
+    String s2 = s1;
+    System.out.println(s1);
+    System.out.println(s2);
+    s1 = "Hello";
+    System.out.println(s1);
+    System.out.println(s2);
   }
 }
