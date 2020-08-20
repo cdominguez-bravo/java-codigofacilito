b663803b197ce6108bbb9d51d2663badc7e54c24
cdominguez_axity <cdominguez.bravo@gmail.com>
2020-08-20 19:10:44 -0400

    curso estructura de datos - codigofacilitio - arreglo como parametro

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,66 @@
+import java.util.Scanner;
+
 public class Main {
 
+  /**INICIO: Curso estructura de datos - seccion arreglo como parametro -> https://codigofacilito.com/videos/arreglo-como-parametro
+   * **/
 
+  public static void modificar(int x){
+    x = 2;
+    System.out.println("metodo modificar - "+x);
+  }
   public static void main(String[] args) {
+
+    //int num[]= {1,2,3,4};
+    int x = 1;
+    modificar(x);
+    System.out.println(x);
+
+  }
+  /**FIN: Curso estructura de datos - seccion arreglo como parametro
+     * **/
+
+    //arreglo de una dimension
+    /*
     int arreglo[] = new int[10];
     arreglo[4] = 9;
     for(int i=0; i < 10; i++){
       System.out.println(arreglo[i]);
     }
     System.out.println(arreglo.length);
-  }
+    */
+
+    //arreglo de 2 dimensiones
+
+    /*
+    int matriz [][] = new int [3][3];
+    int matrizp[][] = {{1,2,3},{4,5,6},{7,8,9}};
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Ingres x e y !!");
+    int x = scanner.nextInt();
+    int y = scanner.nextInt();
+
+    int m[][] = new int [x][y];
+    int n;
+
+    for (int i=0; i <x; i++){
+        for (int j=0;j<y;j++){
+          n = (int)(Math.random()*1000);
+          m[i][j] = n;
+        }
+    }
+
+    //imprimir
+    for (int i=0; i <x; i++){
+      for (int j=0;j<y;j++){
+        System.out.println(m[i][j]);
+      }
+    }
+
+    */
+
+    //int x
+
+
 }
