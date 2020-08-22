9880435bf1219b1e9771fb185855d31dfa2276f7
cdominguez_axity <cdominguez.bravo@gmail.com>
2020-08-21 21:33:26 -0400

    listas enlazadas 2

--- src/ListaEnlazada.java
+++ src/ListaEnlazada.java
@@ -1,5 +1,67 @@
-package PACKAGE_NAME;
-
 public class ListaEnlazada {
+
+  protected Nodo primero;
+
+  public ListaEnlazada(){
+    primero = null;
+  }
+
+  public ListaEnlazada agregarInicio(int dato){
+    Nodo nuevo;
+    nuevo = new Nodo(dato);
+    nuevo.ref = primero;
+    primero = nuevo;
+    return this;
+  }
+
+  public void agregarFinal(int dato){
+    Nodo nuevoF;
+    nuevoF = new Nodo(dato);
+    nuevoF.ref = null;
+    if(primero ==null){
+      primero=nuevoF;
+    }
+
+    Nodo tmp;
+    for(tmp=primero; tmp.getRef()!=null;tmp=tmp.getRef());
+      tmp.setRef(nuevoF);
+
+  }
+
+  public void buscar(){}
+
+  public void eliminar(int dato){
+    boolean encontrado;
+    Nodo actual,anterior;
+    actual = primero;
+    anterior = null;
+    encontrado = false;
+
+    while(actual!=null && !encontrado){
+      encontrado = (actual.dato == dato);
+      if(!encontrado){
+        anterior = actual;
+        actual = actual.getRef();
+      }
+    }
+
+    if(actual!=null){
+      if(actual==primero){
+        primero = actual.ref;
+      }else{
+        anterior.setRef(actual.ref);
+      }
+    }
+
+  }
+
+  public void verLista(){
+    Nodo nodov;
+    nodov = primero;
+    while(nodov != null){
+      System.out.println(nodov.dato);
+      nodov = nodov.ref;
+    }
+  }
 
 }
--- src/Nodo.java
+++ src/Nodo.java
@@ -1,5 +1,22 @@
-package PACKAGE_NAME;
-
 public class Nodo {
+
+  int dato;
+  Nodo ref;
+
+  public Nodo(int d){
+    dato = d;
+  }
+
+  public int getDato(){
+    return dato;
+  }
+
+  public void setRef(Nodo referencia){
+      ref = referencia;
+  }
+
+  public Nodo getRef(){
+    return ref;
+  }
 
 }
